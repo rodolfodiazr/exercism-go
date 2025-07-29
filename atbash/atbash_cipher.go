7816459a22465c80f8fb922ce4138a436370276d
Rodolfo Díaz <rodolfo.diaz.rovira@gmail.com>
2025-07-29 11:26:34 -0500

    Add a solution for Atbash Cipher exercise.

--- atbash/atbash_cipher.go
+++ atbash/atbash_cipher.go
@@ -1,5 +1,30 @@
 package atbash
 
+import (
+	"strings"
+	"unicode"
+)
+
 func Atbash(s string) string {
-	panic("Please implement the Atbash function")
+	var list []rune
+	for _, c := range s {
+		if unicode.IsNumber(c) {
+			list = append(list, c)
+			continue
+		}
+
+		if unicode.IsLetter(c) {
+			list = append(list, 'z'-(unicode.ToLower(c)-'a'))
+		}
+	}
+
+	var builder strings.Builder
+	for i, c := range list {
+		if i > 0 && i%5 == 0 {
+			builder.WriteRune(' ')
+		}
+		builder.WriteRune(c)
+	}
+
+	return builder.String()
 }
